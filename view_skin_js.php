526c304ec1d45f3ad37dddca7fc004eceea11627
aug21 <saug21@gmail.com>
2018-06-23 15:29:10 +0900

    그누보드 메모 게시판 스킨

--- view_skin_js.php
+++ view_skin_js.php
@@ -1,9 +1,11 @@
 <script>
 function comment_tog(name, id) { 
 	var layer = document.getElementById(name+id); 
-	layer.style.display = (layer.style.display == "none")? "block" : "none"; 
-	var comment = document.getElementById("comment"+id);
-	comment.style.display = (comment.style.display == "none")? "block" : "none"; 
+	layer.style.display = (layer.style.display == "none")? "block" : "none";
+	if(name=='edit') {
+		var comment = document.getElementById("comment"+id);
+		comment.style.display = (comment.style.display == "none")? "block" : "none";
+	}
 }
 function comment_delete(url) {
 	if (confirm("이 코멘트를 삭제하시겠습니까?")) location.href = url;
